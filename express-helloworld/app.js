ed175bb48cc3ae19cab359b994d8d807f7aaa63c
hebron nayak <hebron5124@gmail.com>
2023-11-28 10:38:29 +0530

    devenv-version

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,11 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('Hello lover!\n');
+});
+
+app.get('/mars', function(req, res){
+  res.send('happy birthday!\n');
 });
 
 app.listen(8080, function () {
